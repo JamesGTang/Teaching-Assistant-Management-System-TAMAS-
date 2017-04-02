f7480db22d8c7c8129e21b08403f9b36d5dc653a
BijanSadeghi <sadeghi.bijan@gmail.com>
2017-04-01 21:52:29 -0400

    Add files via upload

--- TAMAS/src/ca/mcgill/ecse321/TAMAS/persistence/JobPostingPersistence.java
+++ TAMAS/src/ca/mcgill/ecse321/TAMAS/persistence/JobPostingPersistence.java
@@ -1,29 +1,121 @@
 package ca.mcgill.ecse321.TAMAS.persistence;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class JobPostingPersistence {
 	static String dbURL = "jdbc:mysql://jamesgtang.com:3306/TAMAS_ECSE321";
 	static String user = "TAMAS_DATA_ADMIN";
 	static String pass = "ECSE_ADMIN";
 
-	static int[] post_id = new int[20];
-	static String[] instructor_name = new String[20];
-	static String[] course = new String[20];
-	static String[] job_type = new String[20];
-	static int[] hour = new int[20];
-	static String[] description = new String[20];
-	static int[] mon_st = new int[20];
-	static int[] mon_et = new int[20];
-	static int[] tue_st = new int[20];
-	static int[] tue_et = new int[20];
-	static int[] wed_st = new int[20];
-	static int[] wed_et = new int[20];
-	static int[] thu_st = new int[20];
-	static int[] thu_et = new int[20];
-	static int[] fri_st = new int[20];
-	static int[] fri_et = new int[20];
+	static ArrayList<Integer> post_id = new ArrayList<Integer>();
+	static ArrayList<String> instructor_name = new ArrayList<String>();
+	static ArrayList<String> course = new ArrayList<String>();
+	static ArrayList<String> job_type = new ArrayList<String>();
+	static ArrayList<Integer> hour = new ArrayList<Integer>();
+	static ArrayList<String> description = new ArrayList<String>();
+	static ArrayList<Integer> mon_st = new ArrayList<Integer>();
+	static ArrayList<Integer> mon_et = new ArrayList<Integer>();
+	static ArrayList<Integer> tue_st = new ArrayList<Integer>();
+	static ArrayList<Integer> tue_et = new ArrayList<Integer>();
+	static ArrayList<Integer> wed_st = new ArrayList<Integer>();
+	static ArrayList<Integer> wed_et = new ArrayList<Integer>();
+	static ArrayList<Integer> thu_st = new ArrayList<Integer>(); 
+	static ArrayList<Integer> thu_et = new ArrayList<Integer>();
+	static ArrayList<Integer> fri_st = new ArrayList<Integer>();
+	static ArrayList<Integer> fri_et = new ArrayList<Integer>();
+	static ArrayList<String> days_of_week = new ArrayList<String>();
+	
+	public static void main(String[] args){
+		getJobPostingByCourse("ECSE 222");
+		for(int i=0; i<post_id.size(); i++){
+			System.out.println("Post id: " + post_id.get(i));
+			System.out.println("Instructor: " + instructor_name.get(i));
+			System.out.println("Course: " + course.get(i));
+			System.out.println("Job_type: " + job_type.get(i));
+			System.out.println("Description: " + description.get(i));
+			System.out.println("Monday start time: " + mon_st.get(i));
+			System.out.println("Monday end time: " + mon_st.get(i));
+			System.out.println("Tuesday start time: " + mon_st.get(i));
+			System.out.println("Tuesday end time: " + mon_st.get(i));
+			System.out.println("Wednesday start time: " + mon_st.get(i));
+			System.out.println("Wednesday end time: " + mon_st.get(i));
+			System.out.println("Thursday start time: " + mon_st.get(i));
+			System.out.println("Thursday end time: " + mon_st.get(i));
+			System.out.println("Friday start time: " + mon_st.get(i));
+			System.out.println("Friday end time: " + mon_st.get(i));
+			System.out.println("");
+		}
+	}
+	
+	public ArrayList<Integer> getPostId(){
+		return post_id;
+	}
+	
+	public ArrayList<String> getInstructorName(){
+		return instructor_name;
+	}
+	
+	public ArrayList<String> getCourse(){
+		return course;
+	}
+	
+	public ArrayList<String> getJobType(){
+		return job_type;
+	}
+	
+	public ArrayList<Integer> getHour(){
+		return hour;
+	}
+	
+	public ArrayList<String> getDescription(){
+		return description;
+	}
+	
+	public ArrayList<Integer> getMonSt(){
+		return mon_st;
+	}
+	
+	public ArrayList<Integer> getMonEt(){
+		return mon_et;
+	}
+	
+	public ArrayList<Integer> getTueSt(){
+		return tue_st;
+	}
+	
+	public ArrayList<Integer> getTueEt(){
+		return tue_et;
+	}
+	
+	public ArrayList<Integer> getWedSt(){
+		return wed_st;
+	}
+	
+	public ArrayList<Integer> getWedEt(){
+		return wed_et;
+	}
+	
+	public ArrayList<Integer> getThuSt(){
+		return thu_st;
+	}
+	
+	public ArrayList<Integer> getThuEt(){
+		return thu_et;
+	}
+	
+	public ArrayList<Integer> getFriSt(){
+		return fri_st;
+	}
+	
+	public ArrayList<Integer> getFriEt(){
+		return fri_et;
+	}
 
+	public ArrayList<String> getDaysOfWeek(){
+		return days_of_week;
+	}
+	
 	public static void submitJobPostingtoDB(String instructor_name, String course, String job_type, int hour,
 			String description, String daysofweek, int[] times) {
 		try {
@@ -65,24 +157,25 @@ public class JobPostingPersistence {
 			mystatement.setInt(1, postid);
 			ResultSet myResultSet = mystatement.executeQuery();
 			while (myResultSet.next()) {
-				post_id[0] = myResultSet.getInt("POST_ID");
-				instructor_name[0] = myResultSet.getString("INSTRUCTOR_NAME");
-				course[0] = myResultSet.getString("COURSE");
-				job_type[0] = myResultSet.getString("JOB_TYPE");
-				hour[0] = myResultSet.getInt("HOUR");
-				description[0] = myResultSet.getString("DESCRIPTION");
-				mon_st[0] = myResultSet.getInt("MON_START_TIME");
-				mon_et[0] = myResultSet.getInt("MON_END_TIME");
-				tue_st[0] = myResultSet.getInt("TUE_START_TIME");
-				tue_et[0] = myResultSet.getInt("TUE_END_TIME");
-				wed_st[0] = myResultSet.getInt("WED_START_TIME");
-				wed_et[0] = myResultSet.getInt("WED_END_TIME");
-				thu_st[0] = myResultSet.getInt("THU_START_TIME");
-				thu_et[0] = myResultSet.getInt("THU_END_TIME");
-				fri_st[0] = myResultSet.getInt("FRI_START_TIME");
-				fri_et[0] = myResultSet.getInt("FRI_END_TIME");
-				System.out.println(hour[0]);
-				System.out.println(mon_et[0]);
+				post_id.add(myResultSet.getInt("POST_ID"));
+				instructor_name.add(myResultSet.getString("INSTRUCTOR_NAME"));
+				course.add(myResultSet.getString("COURSE"));
+				job_type.add(myResultSet.getString("JOB_TYPE"));
+				hour.add(myResultSet.getInt("HOUR"));
+				description.add(myResultSet.getString("DESCRIPTION"));
+				mon_st.add(myResultSet.getInt("MON_START_TIME"));
+				mon_et.add(myResultSet.getInt("MON_END_TIME"));
+				tue_st.add(myResultSet.getInt("TUE_START_TIME"));
+				tue_et.add(myResultSet.getInt("TUE_END_TIME"));
+				wed_st.add(myResultSet.getInt("WED_START_TIME"));
+				wed_et.add(myResultSet.getInt("WED_END_TIME"));
+				thu_st.add(myResultSet.getInt("THU_START_TIME"));
+				thu_et.add(myResultSet.getInt("THU_END_TIME"));
+				fri_st.add(myResultSet.getInt("FRI_START_TIME"));
+				fri_et.add(myResultSet.getInt("FRI_END_TIME"));
+				days_of_week.add(myResultSet.getString("DAYS_OF_WEEK"));
+				//System.out.println(hour[0]);
+				//System.out.println(mon_et[0]);
 			}
 		} catch (SQLException e) {
 			// TODO: handle exception
@@ -99,24 +192,25 @@ public class JobPostingPersistence {
 			mystatement.setString(1, name);
 			ResultSet myResultSet = mystatement.executeQuery();
 			while (myResultSet.next()) {
-				post_id[0] = myResultSet.getInt("POST_ID");
-				instructor_name[0] = myResultSet.getString("INSTRUCTOR_NAME");
-				course[0] = myResultSet.getString("COURSE");
-				job_type[0] = myResultSet.getString("JOB_TYPE");
-				hour[0] = myResultSet.getInt("HOUR");
-				description[0] = myResultSet.getString("DESCRIPTION");
-				mon_st[0] = myResultSet.getInt("MON_START_TIME");
-				mon_et[0] = myResultSet.getInt("MON_END_TIME");
-				tue_st[0] = myResultSet.getInt("TUE_START_TIME");
-				tue_et[0] = myResultSet.getInt("TUE_END_TIME");
-				wed_st[0] = myResultSet.getInt("WED_START_TIME");
-				wed_et[0] = myResultSet.getInt("WED_END_TIME");
-				thu_st[0] = myResultSet.getInt("THU_START_TIME");
-				thu_et[0] = myResultSet.getInt("THU_END_TIME");
-				fri_st[0] = myResultSet.getInt("FRI_START_TIME");
-				fri_et[0] = myResultSet.getInt("FRI_END_TIME");
-				System.out.println(description[0]);
-				System.out.println(mon_et[0]);
+				post_id.add(myResultSet.getInt("POST_ID"));
+				instructor_name.add(myResultSet.getString("INSTRUCTOR_NAME"));
+				course.add(myResultSet.getString("COURSE"));
+				job_type.add(myResultSet.getString("JOB_TYPE"));
+				hour.add(myResultSet.getInt("HOUR"));
+				description.add(myResultSet.getString("DESCRIPTION"));
+				mon_st.add(myResultSet.getInt("MON_START_TIME"));
+				mon_et.add(myResultSet.getInt("MON_END_TIME"));
+				tue_st.add(myResultSet.getInt("TUE_START_TIME"));
+				tue_et.add(myResultSet.getInt("TUE_END_TIME"));
+				wed_st.add(myResultSet.getInt("WED_START_TIME"));
+				wed_et.add(myResultSet.getInt("WED_END_TIME"));
+				thu_st.add(myResultSet.getInt("THU_START_TIME"));
+				thu_et.add(myResultSet.getInt("THU_END_TIME"));
+				fri_st.add(myResultSet.getInt("FRI_START_TIME"));
+				fri_et.add(myResultSet.getInt("FRI_END_TIME"));
+				days_of_week.add(myResultSet.getString("DAYS_OF_WEEK"));
+				//System.out.println(description[0]);
+				//System.out.println(mon_et[0]);
 			}
 		} catch (SQLException e) {
 			// TODO: handle exception
@@ -125,37 +219,40 @@ public class JobPostingPersistence {
 	}
 
 	public static void getJobPostingByCourse(String coursename){
-       	try{
-    		Connection myConn = DriverManager.getConnection(dbURL, user, pass);
-    		String preparedsql = "SELECT * FROM  JobPostData WHERE COURSE=?";
-    		PreparedStatement mystatement = myConn.prepareStatement(preparedsql);
-    		mystatement.setString(1, coursename);
-    		ResultSet myResultSet = mystatement.executeQuery();
-    		while(myResultSet.next()){
-    		post_id[0] = myResultSet.getInt("POST_ID");
-    		instructor_name[0]= myResultSet.getString("INSTRUCTOR_NAME");
-    		course[0] = myResultSet.getString("COURSE");
-    		job_type[0]=myResultSet.getString("JOB_TYPE");
-    		hour[0] =myResultSet.getInt("HOUR") ;
-    		description[0] = myResultSet.getString("DESCRIPTION");
-    		mon_st[0]=myResultSet.getInt("MON_START_TIME");
-    		mon_et[0]=myResultSet.getInt("MON_END_TIME");
-    		tue_st[0]=myResultSet.getInt("TUE_START_TIME");
-    		tue_et[0]=myResultSet.getInt("TUE_END_TIME");
-    		wed_st[0]=myResultSet.getInt("WED_START_TIME");
-    		wed_et[0]=myResultSet.getInt("WED_END_TIME");
-    		thu_st[0]=myResultSet.getInt("THU_START_TIME");
-    		thu_et[0]=myResultSet.getInt("THU_END_TIME");
-    		fri_st[0]=myResultSet.getInt("FRI_START_TIME");
-    		fri_et[0]=myResultSet.getInt("FRI_END_TIME");
-    		System.out.println(description[0]);
-    		System.out.println(mon_et[0]);
-    		}
-    		}catch (SQLException e) {
-				// TODO: handle exception
-    			e.printStackTrace();
+		try{
+			Connection myConn = DriverManager.getConnection(dbURL, user, pass);
+			String preparedsql = "SELECT * FROM  JobPostData WHERE COURSE=?";
+			PreparedStatement mystatement = myConn.prepareStatement(preparedsql);
+			mystatement.setString(1, coursename);
+			ResultSet myResultSet = mystatement.executeQuery();
+			int i = 0;
+			while(myResultSet.next()){
+				post_id.add(myResultSet.getInt("POST_ID"));
+				instructor_name.add(myResultSet.getString("INSTRUCTOR_NAME"));
+				course.add(myResultSet.getString("COURSE"));
+				job_type.add(myResultSet.getString("JOB_TYPE"));
+				hour.add(myResultSet.getInt("HOUR"));
+				description.add(myResultSet.getString("DESCRIPTION"));
+				mon_st.add(myResultSet.getInt("MON_START_TIME"));
+				mon_et.add(myResultSet.getInt("MON_END_TIME"));
+				tue_st.add(myResultSet.getInt("TUE_START_TIME"));
+				tue_et.add(myResultSet.getInt("TUE_END_TIME"));
+				wed_st.add(myResultSet.getInt("WED_START_TIME"));
+				wed_et.add(myResultSet.getInt("WED_END_TIME"));
+				thu_st.add(myResultSet.getInt("THU_START_TIME"));
+				thu_et.add(myResultSet.getInt("THU_END_TIME"));
+				fri_st.add(myResultSet.getInt("FRI_START_TIME"));
+				fri_et.add(myResultSet.getInt("FRI_END_TIME"));
+				days_of_week.add(myResultSet.getString("DAYS_OF_WEEK"));
+				//System.out.println(description[i]);
+				//System.out.println(mon_et[i]);
+				i++;
+			}
+		}catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
 		}
-    }
+	}
 
 	public static void getJobPostingByHour(int hourofcourse) {
 		try {
@@ -165,24 +262,25 @@ public class JobPostingPersistence {
 			mystatement.setInt(1, hourofcourse);
 			ResultSet myResultSet = mystatement.executeQuery();
 			while (myResultSet.next()) {
-				post_id[0] = myResultSet.getInt("POST_ID");
-				instructor_name[0] = myResultSet.getString("INSTRUCTOR_NAME");
-				course[0] = myResultSet.getString("COURSE");
-				job_type[0] = myResultSet.getString("JOB_TYPE");
-				hour[0] = myResultSet.getInt("HOUR");
-				description[0] = myResultSet.getString("DESCRIPTION");
-				mon_st[0] = myResultSet.getInt("MON_START_TIME");
-				mon_et[0] = myResultSet.getInt("MON_END_TIME");
-				tue_st[0] = myResultSet.getInt("TUE_START_TIME");
-				tue_et[0] = myResultSet.getInt("TUE_END_TIME");
-				wed_st[0] = myResultSet.getInt("WED_START_TIME");
-				wed_et[0] = myResultSet.getInt("WED_END_TIME");
-				thu_st[0] = myResultSet.getInt("THU_START_TIME");
-				thu_et[0] = myResultSet.getInt("THU_END_TIME");
-				fri_st[0] = myResultSet.getInt("FRI_START_TIME");
-				fri_et[0] = myResultSet.getInt("FRI_END_TIME");
-				System.out.println(description[0]);
-				System.out.println(mon_et[0]);
+				post_id.add(myResultSet.getInt("POST_ID"));
+				instructor_name.add(myResultSet.getString("INSTRUCTOR_NAME"));
+				course.add(myResultSet.getString("COURSE"));
+				job_type.add(myResultSet.getString("JOB_TYPE"));
+				hour.add(myResultSet.getInt("HOUR"));
+				description.add(myResultSet.getString("DESCRIPTION"));
+				mon_st.add(myResultSet.getInt("MON_START_TIME"));
+				mon_et.add(myResultSet.getInt("MON_END_TIME"));
+				tue_st.add(myResultSet.getInt("TUE_START_TIME"));
+				tue_et.add(myResultSet.getInt("TUE_END_TIME"));
+				wed_st.add(myResultSet.getInt("WED_START_TIME"));
+				wed_et.add(myResultSet.getInt("WED_END_TIME"));
+				thu_st.add(myResultSet.getInt("THU_START_TIME"));
+				thu_et.add(myResultSet.getInt("THU_END_TIME"));
+				fri_st.add(myResultSet.getInt("FRI_START_TIME"));
+				fri_et.add(myResultSet.getInt("FRI_END_TIME"));
+				days_of_week.add(myResultSet.getString("DAYS_OF_WEEK"));
+				//System.out.println(description[0];
+				//System.out.println(mon_et[0]);
 			}
 		} catch (SQLException e) {
 			// TODO: handle exception
