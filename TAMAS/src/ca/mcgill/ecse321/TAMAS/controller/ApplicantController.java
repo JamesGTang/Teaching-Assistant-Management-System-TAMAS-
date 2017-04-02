42a7e303866c744b362ecd4d0837ec48639144d7
BijanSadeghi <sadeghi.bijan@gmail.com>
2017-04-01 21:55:41 -0400

    Update ApplicantController.java

--- TAMAS/src/ca/mcgill/ecse321/TAMAS/controller/ApplicantController.java
+++ TAMAS/src/ca/mcgill/ecse321/TAMAS/controller/ApplicantController.java
@@ -18,6 +18,8 @@ import ca.mcgill.ecse321.TAMAS.persistence.JobPostingPersistence;
 public class ApplicantController {
 	private JobApplication ja;
 	private JobApplicationPersistenceController japc = new JobApplicationPersistenceController();
+	Tamas tamas = TamasApplication.getTamas();
+	EceAdmin e = new EceAdmin("Bob", "Bob", "12345", tamas);
 	
 	public ApplicantController(){
 	}
@@ -234,7 +236,130 @@ public class ApplicantController {
 		}
 		
 	}
+
+	/*//for model
+	public List<Course> getListOfCoursesThatHaveOpenPositions(){
+		ArrayList<Course> listOfCourses = new ArrayList<Course>();
+		for (Course course : TamasApplication.getTamas().getCourses()) {
+			// A Course hasOpenPositions when it has at least one Job IsPosted or AppliedTo
+			int openPositions = 0;
+			// Get the Jobs for each course
+			for(Job job : course.getJobs()){
+				String jobState = job.getJobStateFullName();
+				if(jobState.equalsIgnoreCase("IsPosted") || jobState.equalsIgnoreCase("AppliedTo")){
+					openPositions++;
+				}
+			}
+			if (openPositions > 0) {
+				listOfCourses.add(course);				
+			}
+		}
+		return listOfCourses;
+	}
+	//model
+	public List<Job> getAvailableJobs(Course course) {
+		ArrayList<Job> listOfJobs = new ArrayList<Job>();
+		for(Job job : course.getJobs()){
+			String jobState = job.getJobStateFullName();
+			if(jobState.equalsIgnoreCase("IsPosted") || jobState.equalsIgnoreCase("AppliedTo")){
+			// job is open for applications
+				listOfJobs.add(job);
+			}
+		}
+		return listOfJobs;
+	}*/
+
+	//return a list of jobs based on a course code
+	public ArrayList<Job> getJobsByCourse(String courseCode){
+		JobPostingPersistence jp = new JobPostingPersistence();
+		jp.getJobPostingByCourse(courseCode);
+		ArrayList<Job> jobs = new ArrayList<Job>();
+
+		for(int i=0; i<jp.getPostId().size(); i++){
+			int numHours = jp.getHour().get(i);
+			String jobType = jp.getJobType().get(i);
+			boolean isTaJob;
+			if(jobType.equals("TA")){
+				isTaJob = true;
+			}
+			else{
+				isTaJob = false;
+			}
+			boolean isAssigned = false;
+			boolean isAllocated = false;
+			String description = jp.getDescription().get(i);
+			
+			String schedule = "";
+			String delimiters = " ";
+			StringTokenizer st = new StringTokenizer(jp.getDaysOfWeek().get(i), delimiters , true );
+			String token = st.nextToken();
+			while ( st.hasMoreTokens() ) {
+				if(token.equals("MONDAY")){
+					schedule+="MON-" + jp.getMonSt() + "-" + jp.getMonEt() + ";";
+				}
+				if(token.equals("TUESDAY")){
+					schedule+="TUE-" + jp.getTueSt() + "-" + jp.getTueEt() + ";";
+				}
+				if(token.equals("WEDNESDAY")){
+					schedule+="WED-" + jp.getWedSt() + "-" + jp.getWedEt() + ";";
+				}
+				if(token.equals("THURSDAY")){
+					schedule+="THU-" + jp.getThuSt() + "-" + jp.getThuEt() + ";";
+				}
+				if(token.equals("FRIDAY")){
+					schedule+="FRI-" + jp.getFriSt() + "-" + jp.getFriEt() + ";";
+				}
+				token = st.nextToken();
+			}
+			
+		    //Course course = getCourseByCourseCode(courseCode);
+		    Course course = new Course(courseCode, 0, tamas, e);
+			
+		    //can now create the job and add it to jobs
+		    Job job = new Job(numHours, isTaJob, isAssigned, isAllocated, description, schedule, course, tamas);
+		    jobs.add(job);
+		}
+		
+		return jobs;
+	}
 	
+	/*public HashMap<Integer, String> getJobSummariesByCourseCode(String courseCode){
+		ArrayList<Job> jobs = getJobsByCourse(courseCode);
+		HashMap<Integer, String> jobIdAndSummary = new HashMap<Integer, String>();
+		
+		for(Job job : jobs){
+			String summary = "";
+			summary+="Job Type: " + job.getJobTypeFullName() +  "       Hours: " + job.getNumberOfHours() + "       Instructor Name: n/a";
+			jobIdAndSummary.put(job.getJobId(), summary);
+		}
+		
+		return jobIdAndSummary;
+	}*/
 	
+	public String getJobSummaryByJob(Job job){
+		String summary = "";
+		if(job.getJobTypeFullName().equals("TaJob")){
+			summary+="Job Type: TA";
+		}
+		else{
+			summary+="Job Type: Grader";
+		}
+		summary+="\t" + "Hours: " + job.getNumberOfHours() + "\t" + "Instructor Name: n/a";
+		summary = summary.replaceAll("\t", "              ");
+		return summary;
+	}
+	
+	public Course getCourseByCourseCode(String courseCode){
+		//ideally get from persistence (getCourseByCourseCode persistence method)
+		for(Course c : tamas.getCourses()){
+			if(c.getCourseCode().equals(courseCode))
+				return c;
+		}
+		return null;
+	}
+
+	public void applyForJob(Applicant applicant, Job job, String resume){
+
+	}
 	
 }
